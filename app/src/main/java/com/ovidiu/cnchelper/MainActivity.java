fe20b4510d4d373fc2b624aeda13ddfb6c117de6
OvidiuCucu123 <ovidiu.g451@gmail.com>
2023-01-10 17:07:33 +0200

    1. Introducere butoane increment 2. Selectare butoane increment 3. Calcul automat unghi

--- app/src/main/java/com/ovidiu/cnchelper/MainActivity.java
+++ app/src/main/java/com/ovidiu/cnchelper/MainActivity.java
@@ -2,12 +2,13 @@ package com.ovidiu.cnchelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,8 +17,15 @@ public class MainActivity extends AppCompatActivity {
     EditText zstart;
     EditText zfinal;
     TextView afisareUnghi;
-    Button calculate;
+    Button calculateBtn;
+    Button plusBtn;
+    Button minusBtn;
+    ToggleButton toggleBtn;
+    ToggleButton incZO;
+    ToggleButton incZZO;
+    ToggleButton incZZZO;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +36,15 @@ public class MainActivity extends AppCompatActivity {
         zstart = (EditText) findViewById(R.id.z_start);
         zfinal = (EditText) findViewById(R.id.z_final);
         afisareUnghi = (TextView) findViewById(R.id.afisare_unghi_txt);
-        calculate = (Button) findViewById(R.id.calculate_btn);
+        calculateBtn = (Button) findViewById(R.id.calculate_btn);
+        plusBtn = (Button) findViewById(R.id.plus_button);
+        minusBtn = (Button) findViewById(R.id.minus_button);
+        toggleBtn = (ToggleButton) findViewById(R.id.toggle_btn);
+        incZO = (ToggleButton) findViewById(R.id.inczo_btn);
+        incZZO= (ToggleButton) findViewById(R.id.inczzo_btn);
+        incZZZO = (ToggleButton) findViewById(R.id.inczzzo_btn);
 
-    calculate.setOnClickListener(new View.OnClickListener() {
+    calculateBtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             double xsc= Double.parseDouble(xstart.getText().toString());
@@ -39,9 +53,32 @@ public class MainActivity extends AppCompatActivity {
             double zfc= Double.parseDouble(zfinal.getText().toString());
             afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
         }
+    });
+    plusBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            boolean stareButon = toggleBtn.isChecked();
+            double aa = Double.parseDouble(xstart.getText().toString());
+            if (stareButon){
 
+                xstart.setText(Increment(aa));
+                double xsc= Double.parseDouble(xstart.getText().toString());
+                double xfc= Double.parseDouble(xfinal.getText().toString());
+                double zsc= Double.parseDouble(zstart.getText().toString());
+                double zfc= Double.parseDouble(zfinal.getText().toString());
+                afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+
+            }
+        }
+    });
+    minusBtn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
     });
     }
+
 
     public String Calculate(double xs,double xf,double zs,double zf){
         double param, result;
@@ -52,5 +89,47 @@ public class MainActivity extends AppCompatActivity {
         int seconds = (int) ( (result - (double)degree - (double)minutes / 60.0) * 60.0 * 60.0 );
         return "Decimal Degrees: " + result +"°" + "\n"
                 + "Degrees Minutes Seconds: "+ degree +"° " + minutes + "' " + seconds + "'' ";
+    }
+    public String Increment (double nr) {
+        boolean stareinc01 = incZO.isChecked();
+        boolean stareinc001 = incZZO.isChecked();
+        boolean stareinc0001 = incZZZO.isChecked();
+        if (stareinc01 == true && stareinc001 == true && stareinc0001 == true) {
+            return " " + (nr + 0.1 + 0.01 + 0.001);
+        } else if (stareinc01 == true && stareinc001 == true) {
+            return " " + (nr + 0.1 + 0.01);
+        } else if (stareinc01 == true && stareinc0001 == true) {
+            return " " + (nr + 0.1 + 0.001);
+        } else if (stareinc001 == true && stareinc0001 == true) {
+            return " " + (nr + 0.01 + 0.001);
+        } else if (stareinc01 == true) {
+            return " " + (nr + 0.1);
+        } else if (stareinc001 == true){
+            return " " + (nr + 0.01);
+        } else if (stareinc0001 == true){
+            return " " + (nr + 0.001);
+        }
+          else return "error";
+    }
+    public String Decrement (double nr) {
+        boolean stareinc01 = incZO.isChecked();
+        boolean stareinc001 = incZZO.isChecked();
+        boolean stareinc0001 = incZZZO.isChecked();
+        if (stareinc01 == true && stareinc001 == true && stareinc0001 == true) {
+            return " " + (nr - 0.1 - 0.01 - 0.001);
+        } else if (stareinc01 == true && stareinc001 == true) {
+            return " " + (nr - 0.1 - 0.01);
+        } else if (stareinc01 == true && stareinc0001 == true) {
+            return " " + (nr - 0.1 - 0.001);
+        } else if (stareinc001 == true && stareinc0001 == true) {
+            return " " + (nr - 0.01 - 0.001);
+        } else if (stareinc01 == true) {
+            return " " + (nr - 0.1);
+        } else if (stareinc001 == true){
+            return " " + (nr - 0.01);
+        } else if (stareinc0001 == true){
+            return " " + (nr - 0.001);
+        }
+        else return "error";
     }
 }
