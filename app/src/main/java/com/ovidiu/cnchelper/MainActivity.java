e61af8186cc113bc5b738617949ce0be14da234f
OvidiuCucu123 <ovidiu.g451@gmail.com>
2023-01-17 11:21:30 +0200

    Finalizare butoane increment/decrement

--- app/src/main/java/com/ovidiu/cnchelper/MainActivity.java
+++ app/src/main/java/com/ovidiu/cnchelper/MainActivity.java
@@ -19,7 +19,13 @@ public class MainActivity extends AppCompatActivity {
     TextView afisareUnghi;
     Button calculateBtn;
     Button plusBtn;
+    Button plusBtn2;
+    Button plusBtn3;
+    Button plusBtn4;
     Button minusBtn;
+    Button minusBtn2;
+    Button minusBtn3;
+    Button minusBtn4;
     ToggleButton toggleBtn;
     ToggleButton incZO;
     ToggleButton incZZO;
@@ -37,8 +43,14 @@ public class MainActivity extends AppCompatActivity {
         zfinal = (EditText) findViewById(R.id.z_final);
         afisareUnghi = (TextView) findViewById(R.id.afisare_unghi_txt);
         calculateBtn = (Button) findViewById(R.id.calculate_btn);
-        plusBtn = (Button) findViewById(R.id.plus_button);
-        minusBtn = (Button) findViewById(R.id.minus_button);
+        plusBtn = (Button) findViewById(R.id.plus_button1);
+        plusBtn2 = (Button) findViewById(R.id.plus_button2);
+        plusBtn3 = (Button) findViewById(R.id.plus_button3);
+        plusBtn4 = (Button) findViewById(R.id.plus_button4);
+        minusBtn = (Button) findViewById(R.id.minus_button1);
+        minusBtn2 = (Button) findViewById(R.id.minus_button2);
+        minusBtn3 = (Button) findViewById(R.id.minus_button3);
+        minusBtn4 = (Button) findViewById(R.id.minus_button4);
         toggleBtn = (ToggleButton) findViewById(R.id.toggle_btn);
         incZO = (ToggleButton) findViewById(R.id.inczo_btn);
         incZZO= (ToggleButton) findViewById(R.id.inczzo_btn);
@@ -60,25 +72,128 @@ public class MainActivity extends AppCompatActivity {
             boolean stareButon = toggleBtn.isChecked();
             double aa = Double.parseDouble(xstart.getText().toString());
             if (stareButon){
-
                 xstart.setText(Increment(aa));
                 double xsc= Double.parseDouble(xstart.getText().toString());
                 double xfc= Double.parseDouble(xfinal.getText().toString());
                 double zsc= Double.parseDouble(zstart.getText().toString());
                 double zfc= Double.parseDouble(zfinal.getText().toString());
                 afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
-
             }
         }
     });
+
+    plusBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean stareButon = toggleBtn.isChecked();
+                double aa = Double.parseDouble(xfinal.getText().toString());
+                if (stareButon){
+                    xfinal.setText(Increment(aa));
+                    double xsc= Double.parseDouble(xstart.getText().toString());
+                    double xfc= Double.parseDouble(xfinal.getText().toString());
+                    double zsc= Double.parseDouble(zstart.getText().toString());
+                    double zfc= Double.parseDouble(zfinal.getText().toString());
+                    afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+                }
+            }
+    });
+
+    plusBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean stareButon = toggleBtn.isChecked();
+                double aa = Double.parseDouble(zstart.getText().toString());
+                if (stareButon){
+                    zstart.setText(Increment(aa));
+                    double xsc= Double.parseDouble(xstart.getText().toString());
+                    double xfc= Double.parseDouble(xfinal.getText().toString());
+                    double zsc= Double.parseDouble(zstart.getText().toString());
+                    double zfc= Double.parseDouble(zfinal.getText().toString());
+                    afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+                }
+            }
+    });
+
+    plusBtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean stareButon = toggleBtn.isChecked();
+                double aa = Double.parseDouble(zfinal.getText().toString());
+                if (stareButon){
+                    zfinal.setText(Increment(aa));
+                    double xsc= Double.parseDouble(xstart.getText().toString());
+                    double xfc= Double.parseDouble(xfinal.getText().toString());
+                    double zsc= Double.parseDouble(zstart.getText().toString());
+                    double zfc= Double.parseDouble(zfinal.getText().toString());
+                    afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+                }
+            }
+    });
+
     minusBtn.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-
+            boolean stareButon = toggleBtn.isChecked();
+            double aa = Double.parseDouble(xstart.getText().toString());
+            if (stareButon){
+                xstart.setText(Decrement(aa));
+                double xsc= Double.parseDouble(xstart.getText().toString());
+                double xfc= Double.parseDouble(xfinal.getText().toString());
+                double zsc= Double.parseDouble(zstart.getText().toString());
+                double zfc= Double.parseDouble(zfinal.getText().toString());
+                afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+            }
         }
     });
-    }
 
+    minusBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean stareButon = toggleBtn.isChecked();
+                double aa = Double.parseDouble(xfinal.getText().toString());
+                if (stareButon){
+                    xfinal.setText(Decrement(aa));
+                    double xsc= Double.parseDouble(xstart.getText().toString());
+                    double xfc= Double.parseDouble(xfinal.getText().toString());
+                    double zsc= Double.parseDouble(zstart.getText().toString());
+                    double zfc= Double.parseDouble(zfinal.getText().toString());
+                    afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+                }
+            }
+    });
+
+    minusBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean stareButon = toggleBtn.isChecked();
+                double aa = Double.parseDouble(zstart.getText().toString());
+                if (stareButon){
+                    zstart.setText(Decrement(aa));
+                    double xsc= Double.parseDouble(xstart.getText().toString());
+                    double xfc= Double.parseDouble(xfinal.getText().toString());
+                    double zsc= Double.parseDouble(zstart.getText().toString());
+                    double zfc= Double.parseDouble(zfinal.getText().toString());
+                    afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+                }
+            }
+        });
+
+    minusBtn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean stareButon = toggleBtn.isChecked();
+                double aa = Double.parseDouble(zfinal.getText().toString());
+                if (stareButon){
+                    zfinal.setText(Decrement(aa));
+                    double xsc= Double.parseDouble(xstart.getText().toString());
+                    double xfc= Double.parseDouble(xfinal.getText().toString());
+                    double zsc= Double.parseDouble(zstart.getText().toString());
+                    double zfc= Double.parseDouble(zfinal.getText().toString());
+                    afisareUnghi.setText(Calculate(xsc,xfc,zsc,zfc));
+                }
+            }
+    });
+    }
 
     public String Calculate(double xs,double xf,double zs,double zf){
         double param, result;
